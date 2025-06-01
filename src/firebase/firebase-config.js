7d564ed4b2db79e514320bed7fc26bbdbb098ab5
harsh <harshmishra6269@gmail.com>
2025-06-01 15:08:09 +0530

    .env setup

--- src/firebase/firebase-config.js
+++ src/firebase/firebase-config.js
@@ -10,13 +10,13 @@ import { getAuth } from "firebase/auth";
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCkBtbS54Z1r5yCg_iU7okTSMygqWItZD4",
-    authDomain: "shunye-90b28.firebaseapp.com",
-    projectId: "shunye-90b28",
-    storageBucket: "shunye-90b28.appspot.com",
-    messagingSenderId: "214744636774",
-    appId: "1:214744636774:web:844e0a41adac9f00f8fca7",
-    measurementId: "G-2EH3N0CDSK",
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
+    measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
