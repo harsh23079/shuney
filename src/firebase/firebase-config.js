08a133eda0f13c1b4669870433ac8cf0b67453ff
harsh <harshmishra6269@gmail.com>
2025-06-02 11:55:05 +0530

    firebase instance not initialized again and again

--- src/firebase/firebase-config.js
+++ src/firebase/firebase-config.js
@@ -1,5 +1,5 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+import { initializeApp, getApp, getApps } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
@@ -20,7 +20,7 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
 const analytics = getAnalytics(app);
 const db = getFirestore(app);
 const auth = getAuth(app);
